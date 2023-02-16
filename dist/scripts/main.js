e89fe86d8f250e87fa3afc80fe720df33f18140b
Stepan <steapan0606@gmail.com>
2023-02-17 01:50:02 +0700

    Мероприятия

---
+++ dist/scripts/main.js
@@ -0,0 +1,26 @@
+const root = document.documentElement;
+const navToggle = document.querySelector("#js-navToggle");
+navToggle.addEventListener("click", function () {
+  root.classList.toggle("show-nav");
+});
+
+const swipers = document.querySelectorAll(".js-swiper");
+swipers.forEach(function (swpr) {
+  new Swiper(swpr, {
+    updateOnWindowResize: true,
+    slidesPerView: "auto",
+    freeMode: true,
+    spaceBetween: 0,
+    speed: 500,
+    grabCursor: true,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true
+    },
+    navigation: {
+      nextEl: ".swiper-arrow-next",
+      prevEl: ".swiper-arrow-prev",
+      disabledClass: "arrow--disabled"
+    }
+  });
+});
